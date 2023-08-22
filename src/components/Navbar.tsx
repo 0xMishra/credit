c62c746ca3b9ca6d770464ce795a3de8a68e8cd9
mishra811 <mishrakundan073@gmail.com>
2023-08-22 14:14:17 +0530

    added google sign out

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,33 @@
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import { buttonVariants } from './ui/Button'
+import { UserAccountNav } from './UserAccountNav'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions)
   return (
-    <div className='fixed top-0 inset-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+    <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-        <Link href={'/'} className='flex gap-2 items-center'>
-          {/* logo */}
-          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6 ' />
-          <p className='hidden text-zinc-700 text-sm font-medium md:block '>
-            Credit
+        {/* logo */}
+        <Link href='/' className='flex gap-2 items-center'>
+          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+          <p className='hidden text-zinc-700 text-sm font-medium md:block'>
+            Breadit
           </p>
         </Link>
+
         {/* search bar */}
 
-        <Link href={'/sign-in'} className={buttonVariants()}>
-          Sign in
-        </Link>
+        {/* actions */}
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href='/sign-in' className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   )
