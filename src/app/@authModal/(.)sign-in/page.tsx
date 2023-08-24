003e0d807b5b6248d8b8c3ede0b64200df9d11db
mishra811 <mishrakundan073@gmail.com>
2023-08-24 18:36:46 +0530

    added some authoptions

---
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className='fixed inset-0 bg-zinc-900/20 z-10'>
+      <div className='container flex items-center h-full max-w-lg mx-auto'></div>
+    </div>
+  )
+}
+
+export default page
