4d41d585d22295e989d13be398bdbcb400ba90aa
mishra811 <mishrakundan073@gmail.com>
2023-09-09 01:18:49 +0530

    changed main logo icon

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,70 +1,70 @@
-"use client";
+'use client'
 
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
-import { ExtendedPost } from "@/types/db";
-import { useIntersection } from "@mantine/hooks";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Loader2 } from "lucide-react";
-import { FC, useEffect, useRef } from "react";
-import Post from "./Post";
-import { useSession } from "next-auth/react";
-import { Post as Posts } from "@prisma/client";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { ExtendedPost } from '@/types/db'
+import { useIntersection } from '@mantine/hooks'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { Loader2 } from 'lucide-react'
+import { FC, useEffect, useRef } from 'react'
+import Post from './Post'
+import { useSession } from 'next-auth/react'
+import { Post as Posts } from '@prisma/client'
 
 interface PostFeedProps {
-  initialFetchedPosts: Posts[];
-  subredditName?: string;
+  initialFetchedPosts: Posts[]
+  subredditName?: string
 }
 
 export const PostFeed: FC<PostFeedProps> = ({
   initialFetchedPosts,
   subredditName,
 }) => {
-  const initialPosts = initialFetchedPosts as ExtendedPost[];
-  const session = useSession();
-  const lastPostRef = useRef<HTMLElement>(null);
+  const initialPosts = initialFetchedPosts as ExtendedPost[]
+  const session = useSession()
+  const lastPostRef = useRef<HTMLElement>(null)
   const { ref, entry } = useIntersection({
     root: lastPostRef.current,
     threshold: 1,
-  });
+  })
 
   const { data, isFetchingNextPage, fetchNextPage } = useInfiniteQuery(
-    ["infinte-query"],
+    ['infinte-query'],
     async ({ pageParam = 1 }) => {
       const query =
         `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}` +
-        (!!subredditName ? `&subredditName=${subredditName}` : ``);
-      const { data } = await axios.get(query);
-      return data as ExtendedPost[];
+        (!!subredditName ? `&subredditName=${subredditName}` : ``)
+      const { data } = await axios.get(query)
+      return data as ExtendedPost[]
     },
     {
       getNextPageParam: (_, pages) => {
-        return pages.length + 1;
+        return pages.length + 1
       },
       initialData: { pages: [initialPosts], pageParams: [1] },
     },
-  );
+  )
 
   useEffect(() => {
     if (entry?.isIntersecting) {
-      fetchNextPage();
+      fetchNextPage()
     }
-  }, [entry, fetchNextPage]);
+  }, [entry, fetchNextPage])
 
-  const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
+  const posts = data?.pages.flatMap((page) => page) ?? initialPosts
 
   return (
-    <ul className="fliiex flex-col col-span-2 space-y-6">
+    <ul className='fliiex flex-col col-span-2 space-y-6'>
       {posts.map((post, index) => {
         const votesAmt = post.votes.reduce((acc, vote) => {
-          if (vote.type === "UP") return acc + 1;
-          if (vote.type === "DOWN") return acc - 1;
-          return acc;
-        }, 0);
+          if (vote.type === 'UP') return acc + 1
+          if (vote.type === 'DOWN') return acc - 1
+          return acc
+        }, 0)
 
         const currentVote = post.votes.find(
           (vote) => vote.userId === session.data?.user.id,
-        );
+        )
 
         if (index === posts.length - 1) {
           // Add a ref to the last post in the list
@@ -78,7 +78,7 @@ export const PostFeed: FC<PostFeedProps> = ({
                 currentVote={currentVote}
               />
             </li>
-          );
+          )
         } else {
           return (
             <Post
@@ -89,15 +89,15 @@ export const PostFeed: FC<PostFeedProps> = ({
               votesAmt={votesAmt}
               currentVote={currentVote}
             />
-          );
+          )
         }
       })}
 
       {isFetchingNextPage && (
-        <li className="flex justify-center">
-          <Loader2 className="w-6 h-6 text-zinc-500 animate-spin" />
+        <li className='flex justify-center'>
+          <Loader2 className='w-6 h-6 text-zinc-500 animate-spin' />
         </li>
       )}
     </ul>
-  );
-};
+  )
+}
