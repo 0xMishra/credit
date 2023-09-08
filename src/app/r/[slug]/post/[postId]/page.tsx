fbaa2b2e7cc00215b5ac4fcae9ec0ebc27297e01
mishra811 <mishrakundan073@gmail.com>
2023-09-08 14:05:14 +0530

    removed post caching with redis

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -17,32 +17,28 @@ interface SubRedditPostPageProps {
   }
 }
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
 const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
-  const cachedPost = (await redis.hgetall(`post:${params.postId}`)) as CachePost
+  const post = await db.post.findFirst({
+    where: {
+      id: params.postId,
+    },
+    include: {
+      votes: true,
+      author: true,
+    },
+  })
 
-  let post: (Post & { votes: Vote[]; author: User }) | null = null
-
-  if (!cachedPost) {
-    post = await db.post.findFirst({
-      where: {
-        id: params.postId,
-      },
-      include: {
-        votes: true,
-        author: true,
-      },
-    })
-  }
-
-  if (!post && !cachedPost) return notFound()
-
+  if (!post) return notFound()
   return (
     <div>
       <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
         <Suspense fallback={<PostVoteShell />}>
           {/* @ts-expect-error server component */}
           <PostVoteServer
-            postId={post?.id ?? cachedPost.id}
+            postId={post?.id}
             getData={async () => {
               return await db.post.findUnique({
                 where: {
@@ -58,21 +54,21 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
 
         <div className='sm:w-0 w-full flex-1 bg-white p-4 rounded-sm'>
           <p className='max-h-40 mt-1 truncate text-xs text-gray-500'>
-            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{' '}
-            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+            Posted by u/{post?.author.username}{' '}
+            {formatTimeToNow(new Date(post?.createdAt))}
           </p>
           <h1 className='text-xl font-semibold py-2 leading-6 text-gray-900'>
-            {post?.title ?? cachedPost.title}
+            {post?.title}
           </h1>
 
-          <EditorOutput content={post?.content ?? cachedPost.content} />
+          <EditorOutput content={post?.content} />
           <Suspense
             fallback={
               <Loader2 className='h-5 w-5 animate-spin text-zinc-500' />
             }
           >
             {/* @ts-expect-error server component */}
-            <CommentsSection postId={post?.id ?? cachedPost.id} />
+            <CommentsSection postId={post?.id} />
           </Suspense>
         </div>
       </div>
